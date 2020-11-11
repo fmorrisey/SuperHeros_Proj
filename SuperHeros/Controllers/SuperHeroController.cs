9561e1b900465e2191f17e298e9da1d96783f912
Forrest Morrisey <60801874+fmorrisey@users.noreply.github.com>
2020-11-11 11:25:50 -0600

    Inital push of Superheros project. Currently contains model and scaffolding

---
+++ SuperHeros/Controllers/SuperHeroController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeros.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        // GET: SuperHeroController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SuperHeroController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperHeroController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperHeroController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHeroController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHeroController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperHeroController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperHeroController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SuperHeros/Models/SuperHero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeros.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int uId { get; set; }
+
+        public string Name { get; set; }
+
+        public string AlterEgo { get; set; }
+
+        public string PrimaryAbility { get; set; }
+
+        public string SecondaryAbility { get; set; }
+
+        public string CatchPhrase { get; set; }
+
+    }
+}
